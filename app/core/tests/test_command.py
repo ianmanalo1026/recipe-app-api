533451fdbfcb0d68f9a7906b649152c6c90a73f7
Ian Manalo <ian.manalo1026@gmail.com>
2020-11-17 12:12:22 +0000

    Authentication and Auth Token

--- app/core/tests/test_command.py
+++ app/core/tests/test_command.py
@@ -1,8 +1,8 @@
 from unittest.mock import patch
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
+
 
 class CommandTest(TestCase):
     """Test Waiting for db when db is available"""
